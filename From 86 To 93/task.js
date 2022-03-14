f8924a942b80919a6005208ebbb0b8681655aa32
Nourhan Atef <anour2168@gmail.com>
2022-03-14 21:34:39 +0200

    first

---
+++ From 86 To 93/task.js
@@ -0,0 +1,68 @@
+//TASK 1
+
+// let t1 = document.getElementById("elzero");
+
+// let t2 = document.getElementsByClassName("element");
+
+// let t3 = document.getElementsByTagName("div")[0];
+
+// let t4 = document.querySelector("#elzero");
+
+// let t5 = document.querySelector(".element");
+
+// let t6 = document.querySelector("div");
+
+// let t7 = document.querySelector("[name=js]");
+
+// let t8 = document.querySelectorAll("div");
+
+// let t9 = document.querySelectorAll(".element")[0];
+
+// let t10 = document.querySelectorAll("#elzero");
+
+// let t11 = document.querySelectorAll("[name=js]");
+
+// let t12 = document.body.firstElementChild;
+
+//TASK 2
+
+// for (let i = 0; i < document.images.length; i++) {
+//   document.images[i].src =
+//     "https://elzero.org/wp-content/themes/elzero/imgs/logo.png";
+//   document.images[i].alt = "Elzero Logo";
+// }
+
+//TASK 3
+// let inputValue = document.querySelector("[type=number]");
+// let result = document.getElementsByClassName("result")[0];
+
+// inputValue.oninput = (e) => {
+//   result.innerHTML = `{${inputValue.value}} USD Dollar = {${(
+//     inputValue.value * 15.6
+//   ).toFixed(2)}} Egyptian Pound`;
+// };
+
+//TASK 4
+// let firstElem = document.getElementsByClassName("one")[0];
+// let secandElem = document.getElementsByClassName("two")[0];
+// let arr1 = firstElem.getAttribute("title");
+// let arr2 = secandElem.getAttribute("title");
+// let content1 = firstElem.textContent;
+// let content2 = secandElem.textContent;
+// firstElem.textContent = content2;
+// secandElem.textContent = content1;
+// firstElem.setAttribute("title", arr2);
+// secandElem.setAttribute("title", arr1);
+
+//TASK 5
+
+// for (let i = 0; i < document.images.length; i++) {
+//   let image = document.images[i];
+//   if (image.hasAttribute("alt")) {
+//     image.setAttribute("alt", "Old");
+//   } else {
+//     image.setAttribute("alt", "Elzero New");
+//   }
+// }
+
+// TASK 6
