64eac7a13ce2fcb86bd9328f8335a6653109a26e
Nourhan Atef <anour2168@gmail.com>
2022-04-22 13:43:47 +0200

    new tasks

--- From 33 To 37/task.js
+++ From 33 To 37/task.js
@@ -32,7 +32,21 @@ if (num === str) {
 
 //Task 3
 console.log("------- Task 3 -------");
-
-
+let num2 = 10;
+let num3 = 30;
+let num4 = "30";
+num4 > num2 && num4 == num3
+  ? console.log(
+      "30 Is Larger Than 10 And Type string Not The Same Type As number"
+    )
+  : num4 > num2 && num4 == num3 && num4 !== num3
+  ? console.log(
+      "30 Is Larger Than 10 And Value Is The Same As 30 And Type string Not The Same Type As number"
+    )
+  : num4 !== num2 && typeof num4 != typeof num3
+  ? console.log(
+      "{num3} Value And Type Is Not The Same As {num1} And Type Is Not The Same As {num2}"
+    )
+  : console.log("not available ");
 //Task 4
 console.log("------- Task 4 -------");
